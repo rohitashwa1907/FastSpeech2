c7e943c7a71cd483382213a15cd34ce1e6b6ee47
Rohitashwa Chakraborty <46031058+rohitashwa1907@users.noreply.github.com>
2022-01-20 10:16:16 +0530

    Update tools.py

--- utils/tools.py
+++ utils/tools.py
@@ -194,7 +194,7 @@ def synth_samples(targets, predictions, vocoder, model_config, preprocess_config
             stats,
             ["Synthetized Spectrogram"],
         )
-        plt.savefig(os.path.join(path, "{}.png".format(basename)))
+        plt.savefig(os.path.join(path, "Output")+ ".png")
         plt.close()
 
     from .model import vocoder_infer
@@ -207,7 +207,7 @@ def synth_samples(targets, predictions, vocoder, model_config, preprocess_config
 
     sampling_rate = preprocess_config["preprocessing"]["audio"]["sampling_rate"]
     for wav, basename in zip(wav_predictions, basenames):
-        wavfile.write(os.path.join(path, "{}.wav".format(basename)), sampling_rate, wav)
+        wavfile.write(os.path.join(path, "Output")+ ".wav", sampling_rate, wav)
 
 
 def plot_mel(data, stats, titles):
